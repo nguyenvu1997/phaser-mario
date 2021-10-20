2b03c9c2e00c52bbe5bbe7d0321a6699166f0dbe
nguyenvu1997 <nguyenanhvu19971997@gmail.com>
2021-10-20 15:19:32 +0700

    fix bugs

--- src/Scenes/World11.ts
+++ src/Scenes/World11.ts
@@ -87,7 +87,7 @@ export class World11 extends Phaser.Scene {
 
         // Create Player
         const spawnPoint = this.map.findObject("Player", obj => obj.name === "Spawn position");
-        this.player = new Player(this, spawnPoint.x + 2500, spawnPoint.y - 50, 'mario');
+        this.player = new Player(this, spawnPoint.x, spawnPoint.y - 50, 'mario');
         this.player.setPlayerView();
         this.player.setMarioSize('small');
         this.player.chooseAnimation('walk');
